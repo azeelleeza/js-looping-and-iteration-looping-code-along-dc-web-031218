90861cd051a751be368807b48bbbdab5269e9430
azeelleeza <leezaluncheon@gmail.com>
2018-04-23 08:05:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function printBadges(employeeNames) {
+  for (let i = 0; i < employeeNames.length; i++) {
+    console.log(`Welcome ${employeeNames[i]}! You are employee #${i+1}.`);
+  }
+  return employeeNames;
+}
+
+function tailsNeverFails() {
+  tailCount = 0;
+  while(Math.round(Math.random())) {
+    tailCount++;
+  }
+  return `You got ${tailCount} tails in a row!`;
+}
